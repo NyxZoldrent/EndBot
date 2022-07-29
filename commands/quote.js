2b224db25e8ba7818dbc1e9315baba58b930a9c1
NyxZoldrent <r34pco@gmail.com>
2022-07-29 18:09:55 -0500

    forgot to remove unused code from commands/quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -1,11 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-// const homedir = require('os').homedir();
-// const fs = require('node:fs');
-// const path = require('node:path');
-
-// const rawpath = path.join(homedir, 'endbot');
-// const rawdata = fs.readFileSync(homedir + '/endbot/quotes.json');
-// const quotes = JSON.parse(rawdata);
 
 module.exports = {
 	data: new SlashCommandBuilder()
